e180746043e97ca190c79020c6c9f62c5ed380c6
Hemanth Kashyap <SrinivasaHemanth.Tatikonda@cdk.com>
2016-07-07 15:23:29 +0530

    added test code and isGreater method

--- Card.java
+++ Card.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Cards {
-	 char pip;
-	 char suite;
+	  char pip;
+	  char suite;
 	  Cards(char suite,char pip){
 	       this.pip = Character.toUpperCase(pip);
 	       this.suite = suite; 
@@ -26,4 +26,21 @@ public class Cards {
 	 public  String toString(){
 		  return this.suite + ""+this.pip;
 	  }
+	 public boolean isGreater(Cards obj) {
+		Map<Character , Integer> SuitValue = new HashMap<Character,Integer>();
+		SuitValue.put('C',1);
+		SuitValue.put('D',2);
+		SuitValue.put('H',3);
+		SuitValue.put('S',4);
+		if(this.getPip() == obj.getPip()){
+			return (SuitValue.get(this.suite) > SuitValue.get(obj.suite));
+		}else{
+			return (this.getPip() > obj.getPip());
+		}
+	}
+	public static void main(String args[]) {
+		Cards first = new Cards('s','8');
+		Cards second = new Cards('k','8');
+		System.out.println(first.isGreater(second));
+	}
 }
