14582d5a36a7cc2b98dd156177a4d424a7dba75e
DivyaManasaGC <DivyaManasa.GuruCharan@cdk.com>
2016-07-08 15:47:23 +0530

    Delete test.java

--- test.java
+++
@@ -1,8 +0,0 @@
-
-public class testCard {
-	public static void main(String args[]) {
-		Cards first = new Cards('s','8');
-		Cards second = new Cards('k','8');
-		System.out.println(first.isGreater(second));
-	}
-}
